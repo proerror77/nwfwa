bf439e71376fe5a9e633d4c7c56ba1e4824c30a9
Wild Card <proerror77@users.noreply.github.com>
2026-06-08 22:14:54 +0800

    Split Medical Review web console page

--- apps/web-console/src/pages/mod.rs
+++ apps/web-console/src/pages/mod.rs
@@ -4,6 +4,7 @@ mod data_sources;
 mod evidence_hub;
 mod evidence_runtime;
 mod hubs;
+mod medical_review;
 mod runtime_scoring;
 
 pub use bootstrap_ops::*;
@@ -12,4 +13,5 @@ pub use data_sources::*;
 pub use evidence_hub::*;
 pub use evidence_runtime::*;
 pub use hubs::*;
+pub use medical_review::*;
 pub use runtime_scoring::*;
